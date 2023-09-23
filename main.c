abe85cc0e0a109575dd53a72085fcf67e8326a05
Ernest Gupik <ernestgupik@wp.pl>
2023-09-23 17:18:05 +0200

    Render file tree and readme in the repo endpoint

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 #include <git2.h>
 #include <stdio.h>
+#include <dirent.h>
 
 #define PORT 8080
 #define REPO_PATH "."
@@ -76,6 +77,64 @@ commit_array* get_commit_messages(git_oid *commit_oid) {
     return commits;
 }
 
+char* read_to_string(const char* file_path) {
+    FILE* file = fopen(file_path, "r");
+    if (file == NULL) {
+        return NULL;
+    }
+
+    fseek(file, 0, SEEK_END);
+    long file_size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    char* content = malloc(file_size + 1);
+    if (content == NULL) {
+        fclose(file);
+        return NULL;
+    }
+
+    size_t bytes_read = fread(content, 1, file_size, file);
+    content[bytes_read] = '\0';
+
+    fclose(file);
+    return content;
+}
+
+char* get_readme() {
+    // TODO: render readme
+    // TODO: case insensitive readme filename
+    char* readme_content = read_to_string("README.md");
+    if (!readme_content) {
+        return "README.md file not found";
+    }
+    return readme_content;
+}
+
+char* get_file_tree() {
+    struct dirent *de;
+
+    DIR* dr = opendir("."); // current directory
+    if (!dr) {
+        return NULL;
+    }
+
+    char* file_tree = malloc(100000);
+    strcat(file_tree, "<pre><h2>File Tree</h2><ul>");
+
+    while ((de = readdir(dr)) != NULL) {
+        if (strcmp(de->d_name, ".") != 0 && strcmp(de->d_name, "..") != 0) {
+            strcat(file_tree, "<li>");
+            strcat(file_tree, de->d_name);
+            strcat(file_tree, "</li>");
+        }
+    }
+
+    strcat(file_tree, "</ul></pre>");
+
+    closedir(dr);
+    return file_tree;
+}
+
 /* ENDPOINTS */
 
 struct http_response_s* root_endpoint() {
@@ -97,11 +156,19 @@ struct http_response_s* repo_endpoint() {
     git_oid head_oid = *git_reference_target(head_ref);
     commit_array* commits = get_commit_messages(&head_oid);
 
+    char* text = malloc(100000);
+
     struct http_response_s* response = http_response_init();
     http_response_status(response, 200);
     http_response_header(response, "Content-Type", "text/html");
-    char* context[] = {"", repo_template[0], NULL};
-    char* text = template_render(base_template, context);
+
+    char* readme_content = get_readme();
+    strcat(text, "<pre>");
+    strcat(text, readme_content);
+    strcat(text, "</pre>");
+
+    char* file_tree = get_file_tree();
+    strcat(text, file_tree);
 
     char buf[1000];
     strcat(text, "<table><thead><tr><th>Commit ID</th><th>Commit Message</th></tr></thead><tbody>");
@@ -110,8 +177,6 @@ struct http_response_s* repo_endpoint() {
         strcat(text, buf);
     }
     strcat(text, "</tbody></table>");
-
-    http_response_body(response, text, strlen(text));
 
     // Free the memory
     for (int i = 0; i < commits->size; ++i) {
@@ -122,13 +187,24 @@ struct http_response_s* repo_endpoint() {
     free(head_ref);
     /* free(text); */ // causes memory corruption (!?) (C ...)
 
+    char* context[] = {"", text, NULL};
+    char* resp_body = template_render(base_template, context);
+    http_response_body(response, resp_body, strlen(resp_body));
+
     return response;
+}
+
+void trim_querystring(char* s) {
+    strtok(s, "?");
 }
 
 /* SERVER CODE */
 
 void handle_request(struct http_request_s* request) {
     char* url = http_request_path(request);
+    struct http_string_s query_str = http_request_querystring(request);
+
+    trim_querystring(url);
 
     struct http_response_s* response;
     if (strcmp(url, "/") == 0) {
